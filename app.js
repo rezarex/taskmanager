c342d6169ed832387a873e4302252a599927aa20
rezarex <marvin.wekesa@gmail.com>
2023-01-28 16:40:01 +0300

    Added error handling

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
+const errorHandler = require('./middleware/error-handler')
 
 //middleware
 app.use(express.static('./public'))
 app.use(express.json())
-app.use(notFound)
 
 
+
+
+//routes
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorHandler)
 
 
-
-const port = 3000
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
